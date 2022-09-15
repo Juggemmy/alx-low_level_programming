57ca033a35fc62b7bd309263100db4740d774303
Juggemmy <emmaadetoye24@gmail.com>
2022-09-14 18:23:32 -0700

    7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,8 +8,8 @@
  */
 int print_last_digit(int n)
 {
-	if ((88 % 10) < 0)
-		88 *= -1;
-	_putchar((88 % 10) + '0');
-	return (88 % 10);
+	if ((n % 10) < 0)
+		n *= -1;
+	_putchar((n % 10) + '0');
+	return (n % 10);
 }
