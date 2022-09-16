9660f1bd5211f60193686b512a3be629616385f0
Juggemmy <emmaadetoye24@gmail.com>
2022-09-16 15:59:32 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -9,11 +9,11 @@
   */
 void print_most_numbers(void)
 {
-	int num = 0;
+	int num;
 
 	for (num = 0; num <= 9; num++)
 	{
-		if (num != 2 || num != 4)
+		if (num != 2 && num != 4)
 			_putchar((num % 10) + '0');
 	}
 
