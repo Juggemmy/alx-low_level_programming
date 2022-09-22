d29aa53fa2239b873854b29cf851365f73529682
Juggemmy <emmaadetoye24@gmail.com>
2022-09-22 16:55:20 -0700

    104-print_buffer.c

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -20,8 +20,10 @@ void print_buffer(char *b, int size)
 		{
 			if ((index + byte) >= size)
 				printf(" ");
+
 			else
 				printf("%02x", *(b + index + byte));
+
 			if ((index % 2) != 0 && index != 0)
 				printf(" ");
 		}
@@ -30,15 +32,19 @@ void print_buffer(char *b, int size)
 		{
 			if ((index + byte) >= size)
 				break;
+
 			else if (*(b + index + byte) >= 31 &&
 				 *(b + index + byte) <= 126)
 				printf("%c", *(b + index + byte));
+
 			else
 				printf(".");
 		}
 
 		if (byte >= size)
 			continue;
+
+		printf("\n");
 	}
 
 	if (size <= 0)
