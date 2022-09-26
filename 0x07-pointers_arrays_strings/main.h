3d29b847daea9d8c36cf3a333c6d4eb82608f4ed
Juggemmy <emmaadetoye24@gmail.com>
2022-09-26 07:36:22 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * Desc: Header file containing prototypes for all functions
+ * 	 used in the 0x07-pointers_arrays_strings directory
+ */
+
+char *_memset(char *s, char b, unsigned int n);
+
+#endif /*MAIN_H*/
