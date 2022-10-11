17b4dd02bc537c90f1159e091a240c3be5f665cb
Juggemmy <emmaadetoye24@gmail.com>
2022-10-11 11:54:56 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,23 @@
+#ifndef _HEADER_
+#define _HEADER_
+
+/**
+ * struct dog - Dog attributes
+ * @name: Dog name
+ * @age: Dog age
+ * @owner: Dog owner
+ *
+ * Desc: Dog attributes
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ * dog_t - Typedef for dog structure
+ */
+
+#endif
