59efb240245ccb0ba3604ae2143bacce59cf1669
Juggemmy <emmaadetoye24@gmail.com>
2022-10-11 12:23:48 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,7 +2,7 @@
 #include "dog.h"
 
 /**
- * init_dog - Initilizes a dog structure
+ * init_dog - Initializes a dog structure
  * @d: A dog structure.
  * @name: The name of the dog.
  * @age: The age of the dog.
