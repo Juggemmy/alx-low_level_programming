c4749bf5ebd8358f02817b25897664a8faa54deb
Juggemmy <emmaadetoye24@gmail.com>
2022-10-13 13:41:42 -0700

    100-main_opcodes.c

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,6 @@
 #ifndef _HEADER_
 #define _HEADER_
 #include <stdlib.h>
-#include <udis86.h>
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
