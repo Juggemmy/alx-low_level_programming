77a1e62fa92f9f7ee82da934dd6d20b189ff937d
Juggemmy <emmaadetoye24@gmail.com>
2022-10-21 15:19:10 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,14 +10,14 @@
  */
 size_t print_list(const list_t *h)
 {
-	size_t count;
+   size_t count;
 
-	if (h == NULL)
-		return (0);
-	count = 1;
-		printf("[%d] %s\n", h->len, h->str);
-	if (h->next != NULL)
-		count += print_list(h->next);
-	return (count);
+   if (h == NULL)
+	   return (0);
+   count = 1;
+   printf("[%d] %s\n", h->len, h->str);
+   if (h->next != NULL)
+      count += print_list(h->next);
+   return (count);
 }
 
